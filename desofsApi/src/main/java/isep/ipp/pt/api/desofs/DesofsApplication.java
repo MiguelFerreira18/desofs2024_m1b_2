b7f520a65155f47c2c298e31b0ac20659f80a2fe
Miguel[1230199] <1230199@isep.ipp.pt>
2024-04-29 11:45:14 +0100

    [feat] dockerize project

--- desofsApi/src/main/java/isep/ipp/pt/api/desofs/DesofsApplication.java
+++ desofsApi/src/main/java/isep/ipp/pt/api/desofs/DesofsApplication.java
@@ -9,24 +9,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DesofsApplication {
-	@Value("${spring.datasource.url}")
-	private static String url;
-
-	@Value("${spring.datasource.username}")
-	private static String username;
-
-	@Value("${spring.datasource.password}")
-	private static String password;
 
 	private static final Logger logger = LoggerFactory.getLogger(DesofsApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(DesofsApplication.class, args);
-		logger.info("Application started successfully");
-		logger.info(url);
-		logger.info(username);
-		logger.info(password);
-		logger.info("Application started successfully");
 
 	}
 
