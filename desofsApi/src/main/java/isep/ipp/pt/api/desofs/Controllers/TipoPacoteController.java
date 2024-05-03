da5698ccfee0ac3ba3bb4e0c7753567303ddb846
Miguel[1230199] <1230199@isep.ipp.pt>
2024-05-03 14:41:06 +0100

    [fix]: fix request mapping

--- desofsApi/src/main/java/isep/ipp/pt/api/desofs/Controllers/TipoPacoteController.java
+++ desofsApi/src/main/java/isep/ipp/pt/api/desofs/Controllers/TipoPacoteController.java
@@ -13,7 +13,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController("/tipoPacote")
+@RestController
+@RequestMapping("/tipoPacote")
 public class TipoPacoteController {
     @Autowired
     TipoPacoteService tipoPacoteService;
